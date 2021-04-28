6dd8eceb4e01775a00ac84311b0c1a9680f8f5a9
Tatiana Barbosa <83253918+tatbarb@users.noreply.github.com>
2021-04-28 17:08:29 -0300

    Add files via upload

---
+++ Cadastro.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Cadastro{
+
+	public static void main (String args[]){
+
+	Scanner form = new Scanner(System.in);
+
+	System.out.println("************************");
+	System.out.println("**** SEJA BEM-VINDO ****");
+	System.out.println("************************");
+
+
+
+	System.out.println("Informe seu nome:");
+	String nome = form.nextLine();
+
+
+	System.out.println("Informe seu cpf:");
+	String cpf = form.nextLine();
+
+
+	System.out.println("Informe sua idade:");
+	int idade = form.nextInt();
+
+
+	System.out.println("Informe sua altura:");
+	double altura = form.nextDouble();
+
+		System.out.println();
+		System.out.println("*****************************");
+		System.out.println("NOME: " + nome);
+		System.out.println("CPF: " + cpf);
+		System.out.println("IDADE: " + idade);
+		System.out.println("ALTURA: " + altura);
+		System.out.println("*****************************");
+	}//end main
+}//end class Cadastro
+
+
